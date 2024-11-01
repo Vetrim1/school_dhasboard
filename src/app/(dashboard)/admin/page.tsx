e1f3730661e728ec5eaaa1a11376578c900a1525
Vetrim1 <velvetribe223@gmail.com>
2024-11-01 15:56:28 +0530

    develop user cards

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,21 @@
+import UserCard from '@/components/UserCard';
 import React from 'react';
 
 export default function page() {
   return (
-    <div>Admin page</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* Left */}
+      <div className='w-full lg:w-2/3'>
+      <div className="flex gap-4 justify-between flex-wrap">
+        <UserCard type="student"/>
+        <UserCard type="teacher"/>
+        <UserCard type="parent"/>
+        <UserCard type="staff"/>
+        
+      </div>
+      </div>
+      {/* Right */}
+      <div className='w-full lg:w-1/3'>Right</div>
+    </div>
   )
 }
